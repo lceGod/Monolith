49a9488038d9c39e7532f42e25b2605ddaea5b58
Adel <adelfat71@mail.ru>
2023-12-14 19:41:15 +0400

    Add comments

--- ASMF Monolith/PlacementService.cs
+++ ASMF Monolith/PlacementService.cs
@@ -11,13 +11,13 @@ namespace ASMF_Monolith
         public readonly Placement placement;
         private DataBase db;
 
-        public PlacementService(string name)
+        public PlacementService(string name) // Конструктор класса
         {
             db = new DataBase();
             placement = db.GetPlacement(name);
         }
 
-        public void AddMaterial(string name, string unit)
+        public void AddMaterial(string name, string unit) //Метод добавления материала в помещение
         {
             var material = new Material(name, 0, unit, 0);
 
@@ -25,13 +25,13 @@ namespace ASMF_Monolith
             placement.Materials.Add(material);
         }
 
-        public void DeleteMaterial(int materialIndex)
+        public void DeleteMaterial(int materialIndex) // Метод удаления материала из помещения 
         {
             placement.Materials.RemoveAt(materialIndex);
             db.UpdateMaterailList(placement.Name, placement.Materials);
         }
 
-        public bool TryChangeMaterialCount(double count, int materialIndex) 
+        public bool TryChangeMaterialCount(double count, int materialIndex) // Метод изменения количества материала 
         {
             if (placement.Materials[materialIndex].Count + count < 0) 
             {
