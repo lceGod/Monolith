8db37f124cc71267d1c70d166b1c882ed5080e0a
IceGod <rost.post1718@gmail.com>
2023-12-14 19:11:49 +0400

    add Form for add new placement

---
+++ ASMF Monolith/AddPlacementForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASMF_Monolith
+{
+    public partial class AddPlacementForm : Form
+    {
+        public AddPlacementForm()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var name = nameBox.Text;
+            var password = pwBox.Text;
+
+            if (name == "" || password == "")
+            {
+                MessageBox.Show("Вначалее введите название и пароль помещения");
+                return;
+            }
+
+            if (ManagerService.PlacementExist(name))
+            {
+                MessageBox.Show("Помещение с таким названием уже существует");
+                return;
+            }
+
+            ManagerService.AddPlacement(name, password, false);
+            this.Close();
+        }
+    }
+}
