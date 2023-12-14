408ba069d154b88a5fc1948ff9aa98b50081fe94
Adel <adelfat71@mail.ru>
2023-12-14 17:46:20 +0400

    Realize connection

---
+++ ASMF Monolith/DataBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.Logging;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
+using MySqlX.XDevAPI.Relational;
+
+namespace MonolithMainForm
+{
+    class DataBase
+    {
+        static readonly MySqlConnection connection = new(
+            "server=localhost;" +
+            "port=3306;" +
+            "user=root;" +
+            "password=root;" +
+            "database=monolith");
+
+        public void OpenConection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void CloseConection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
