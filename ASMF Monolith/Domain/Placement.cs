a95f38142733d00f502f430fe67ca72f0c5ddb71
Adel <adelfat71@mail.ru>
2023-12-14 17:02:23 +0400

    Add Class Placement

---
+++ ASMF Monolith/Domain/Placement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonolithMainForm
+{
+    class Placement
+    {
+        public String Name { get; set; }
+        public List<Material> Materials { get; set; }
+        public bool IsStorage { get; set; }
+
+        public Placement(string login, bool isStorage) 
+        {
+            this.Name = login;
+            this.IsStorage = isStorage;
+        }
+
+    }
+}
