e6ffbeb4cacc5cc7117138b9b07a7895f60a77f7
Adel <adelfat71@mail.ru>
2023-12-14 16:59:16 +0400

    Add Class Matials

---
+++ ASMF Monolith/Domain/Material.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonolithMainForm
+{
+    class Material
+    {
+        public string Name { get; set; }
+
+        private double count;
+        public double Count 
+        { 
+            get { return count; } 
+            set
+            {
+                if (value < 0) throw new Exception("ОТРИЦАТЕЛЬНОЕ ЗНАЧЕНИЕ");
+                count = value; 
+            }
+        }
+        private double limit;
+        public double Limit
+        {
+            get { return limit;}
+            set
+            {
+                if (value < 0) throw new Exception("ОТРИЦАТЕЛЬНОЕ ЗНАЧЕНИЕ");
+                limit = value;
+            }
+        }
+        public string Unit { get; set; }
+
+        public Material(string name, double count, string unit, double limit) 
+        {
+            this.Name = name;
+            this.Count = count;
+            this.Unit = unit;
+            this.Limit = limit;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2}", Name, Math.Round(Count, 2), Unit);
+        }
+
+    }
+}
