17a78b832018f3b9479965cac02a918d8a443f1c
Adel <adelfat71@mail.ru>
2023-12-14 19:38:28 +0400

    Add Class PlacementService

---
+++ ASMF Monolith/PlacementService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMF_Monolith
+{
+    class PlacementService
+    {
+        public readonly Placement placement;
+        private DataBase db;
+
+        public PlacementService(string name)
+        {
+            db = new DataBase();
+            placement = db.GetPlacement(name);
+        }
+
+        public void AddMaterial(string name, string unit)
+        {
+            var material = new Material(name, 0, unit, 0);
+
+            db.AddMaterial(material, placement.Name);
+            placement.Materials.Add(material);
+        }
+
+        public void DeleteMaterial(int materialIndex)
+        {
+            placement.Materials.RemoveAt(materialIndex);
+            db.UpdateMaterailList(placement.Name, placement.Materials);
+        }
+
+        public bool TryChangeMaterialCount(double count, int materialIndex) 
+        {
+            if (placement.Materials[materialIndex].Count + count < 0) 
+            {
+                return false;
+            }
+
+            placement.Materials[materialIndex].Count += count;
+            db.UpdateMaterailList(placement.Name, placement.Materials);
+
+            return true;
+        }
+    }
+}
