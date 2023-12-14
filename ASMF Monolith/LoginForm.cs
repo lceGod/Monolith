5ba21ccfa39af47f4451d3396bf8565d89b2f867
IceGod <rost.post1718@gmail.com>
2023-12-14 18:03:42 +0400

    create code structure
    Wait realisation Database and Other Window

--- ASMF Monolith/LoginForm.cs
+++ ASMF Monolith/LoginForm.cs
@@ -20,35 +20,40 @@ namespace MonolithMainForm
 
         private void SignButton_Click(object sender, EventArgs e)
         {
-            var login = loginField.Text.Trim();
-            var password = passField.Text.Trim();
+            var login = loginField.Text.Trim(); // Логин введенный пользователем 
+            var password = passField.Text.Trim(); // Пароль введенный пользователем
 
-            //if (login == "admin" && password == "admin")
-            //{
+
+            // Вход в функционал менеджера
+            if (login == "admin" && password == "admin") 
+            {
                 //var mf = new ManagerForm();
                // mf.Show();
                 //this.Close();
                 //return;
-            //}
+            }
 
+            // Создание БД и таблицы для хранения данных
             //var db = new DataBase();
-
             //var table = new DataTable();
             //var adapter = new MySqlDataAdapter();
-            //var command = new MySqlCommand("SELECT * FROM placements WHERE name = @login AND password = @pass", db.GetConection());
 
+            // Создание запроса, на выбор из таблицы запесей с нужной нам парой логин-пароль
+            //var command = new MySqlCommand("SELECT * FROM placements WHERE name = @login AND password = @pass", db.GetConection());
             //command.Parameters.Add("@login", MySqlDbType.VarChar).Value = login;
             //command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = password;
 
+            // Реализация запроса, заполнение таблица выбранной информацией
             //adapter.SelectCommand = command;
             //adapter.Fill(table);
-
+            
+            // Если находим подходящее нам значение, то даем доступ к функционалу Помещения
             //if (table.Rows.Count > 0)
             //{
                 //var pf = new PlacementForm(login);
                 //pf.Show();
             //}
-            //else
+            //else // Иначе выводим сообщение о неверной паре логин-пароль
                 //MessageBox.Show("Неверная пара значений Логин-пароль");
 
         }
